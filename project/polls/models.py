d5e92644547041941e9f30ca16afbae5004530d2
Rj Bernaldo <rj.bernaldo@gmail.com>
2015-09-04 09:50:46 +0800

    Removes some unused commented out code

--- project/polls/models.py
+++ project/polls/models.py
@@ -3,8 +3,6 @@ from django.utils import timezone
 
 import datetime
 
-# Create your models here.
-
 class Poll(models.Model):
   question = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
